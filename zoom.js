9a0a0f85a8c83103a00a8d70291209c07d32542d
Sinelnikov Roman <sinelnikov.rv@gmail.com>
2015-07-16 12:55:34 +0300

    bootstrap

--- zoom.js
+++ zoom.js
@@ -1,3 +1,19 @@
-/**
- * Created by uzer on 13.07.2015.
- */
+$('img').click(function () {
+    var zoomable = $(this);
+    var clickedElemHTML = zoomable[0].outerHTML;
+    if ($('.zoom').length) {
+        var zoomableElemntHTML = $('.zoom').children()[0].outerHTML;
+        if (clickedElemHTML === zoomableElemntHTML) {
+            $('.zoom').hide(500, function () {
+                $('.zoom').remove();
+            });
+        } else {
+            $('.zoom').hide(500, function () {
+                $('.zoom').remove();
+                zoomable.parent().after('<div class="zoom">' + clickedElemHTML + '</div>').slideDown(400);
+            });
+        }
+    } else {
+        zoomable.parent().after('<div class="zoom">' + clickedElemHTML + '</div>').slideDown(400);
+    }
+});
